cdc68bb3c2a73685b2c77b76047fca0f355d6ea4
vale75020 <valentinosuma@hotmail.com>
2019-10-22 19:15:15 +0200

    fix bugs css

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,60 +1,68 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Photos extends Component {
-    state = {
-        photos:[],
-        user:""
-    }
-    
-      componentDidMount() {
-        axios
-        .get("https://randomuser.me/api/?results=80")
-        .then(response => {
-          console.log(response)
-          this.setState({
-            photos: response.data.results
-          });
-          console.log(this.state.photos);
-        })
-        .catch(error => {
-          console.log(error);
+  state = {
+    photos: [],
+    user: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?results=80")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          photos: response.data.results
         });
-      }
-handleChange = (e) => {
+        console.log(this.state.photos);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  handleChange = e => {
     this.setState({
-        user: e.target.value
-    })
-    console.log(this.state.user)
-}
+      user: e.target.value
+    });
+    console.log(this.state.user);
+  };
 
-handleSubmit = (e, name) => {
+  handleSubmit = (e, name) => {
     e.preventDefault();
-    let findUser = this.state.photos.filter(x => x.name === this.state.user)
-    console.log(findUser)
+    let findUser = this.state.photos.filter(x => name === x.name.last);
+    console.log(findUser);
     this.setState({
-        photos: findUser, user:""
-    })
+      photos: findUser,
+      user: ""
+    });
+  };
 
-}
-      
-    render() {
-        return (
-            <div>
-                <h1>Users</h1>
-                <input className="input-user" type="text" placeholder="search user" style={{borderRadius:"20px"}} value={this.state.user} 
-                onChange={this.handleChange} onSubmit={this.handleSubmit}/>
-                {this.state.photos.map(photo => {
-                    return(
-                        <div className="photo" key={photo.login.uuid}>
-                            <img src={photo.picture.medium} alt={photo.title} />
-                            <h4>{photo.name.title} {photo.name.first} {photo.name.last}</h4>
-                            <h5>{photo.email}</h5>
-                            <p></p>
-                        </div>
-                    )
-                })}
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <input
+          className="input-user"
+          type="text"
+          placeholder="search user"
+          value={this.state.user}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+        />
+        {this.state.photos.map(photo => {
+          return (
+            <div className="photo" key={photo.login.uuid}>
+              <img src={photo.picture.medium} alt={photo.title} />
+              <h4>
+                {photo.name.title} {photo.name.first} {photo.name.last}
+              </h4>
+              <h5>{photo.email}</h5>
+              <p></p>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
